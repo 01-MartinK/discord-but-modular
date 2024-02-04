31c6eaf7c0cb4f8cf7285a52bf6ac898b9cc8d48
Martin K <thedeadgamer101@gmail.com>
2024-02-04 12:09:52 +0200

    Main Server Sidebar buttons for interaction/ Moderator and Admin buttons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import { ClerkProvider } from "@clerk/nextjs";
-
-import type { Metadata } from "next";
-import "./globals.css";
 import { Open_Sans } from 'next/font/google';
-import { ThemeProvider } from "@/components/providers/theme-provider";
+import type { Metadata } from "next";
+
+import "./globals.css";
+
 import { cn } from "@/lib/utils";
+
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const font = Open_Sans({ subsets: ['latin']});
 
@@ -31,6 +34,7 @@ export default function RootLayout({
             enableSystem={true}
             storageKey="nexus-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
